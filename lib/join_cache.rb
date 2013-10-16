3c10697bb4a50e51f37b815b8b2df58f2599b5a5
Kevin Bongart <contact@kevinbongart.net>
2013-10-16 17:06:59 -0400

    Avoid joins completely by querying the join table directly

--- lib/join_cache.rb
+++ lib/join_cache.rb
@@ -18,15 +18,18 @@ module JoinCache
     # => Team.where(id: [4, 8, 15, 16, 23, 42])
     #
     reflect_on_all_associations(:has_and_belongs_to_many).each do |association|
-      singular_name   = association.name.to_s.singularize # team
-      plural_name     = association.plural_name           # teams
-      cached_name     = "cached_#{plural_name}"           # cached_teams
-      cached_ids_name = "cached_#{singular_name}_ids"     # cached_team_ids
+      singular_name   = association.name.to_s.singularize         # team
+      plural_name     = association.plural_name                   # teams
+      cached_name     = "cached_#{plural_name}"                   # cached_teams
+      cached_ids_name = "cached_#{singular_name}_ids"             # cached_team_ids
+      foreign_key     = association.foreign_key                   # employee_id
+      join_table      = association.join_table                    # employees_teams
+      join_model      = join_table.classify.pluralize.constantize # EmployeesTeams
 
       # cached_team_ids
       define_method(cached_ids_name) do
         Rails.cache.fetch("#{cache_key}/#{cached_ids_name}") do
-          send(plural_name).pluck(self.class.name.foreign_key.to_sym)
+          join_model.where(foreign_key => id).pluck(foreign_key.to_sym)
         end
       end
 
